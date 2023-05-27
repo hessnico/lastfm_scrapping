a05b416aed5746bd34e6f7304e5ec59372ac9d86
Nícolas Hess <nicolashess1@gmail.com>
2023-05-27 20:03:21 -0300

    improved function get_last_library_page

--- modules.py
+++ modules.py
@@ -5,17 +5,36 @@ import csv
 import datetime
 import sys
 
-def get_last_library_page(soup):
+def get_last_library_page(user):
+    page = requests.get(f'https://www.last.fm/pt/user/{user}/library')
+    soup = BeautifulSoup(page.content, "html.parser")
     results = soup.find(class_="col-main")
     uls = results.select('nav.pagination')
 
     lis = [li.text for ul in uls for li in ul.findAll('li')]
     last_page_to_search = int(''.join(filter(str.isdigit, lis[-2])))
+
     return last_page_to_search
 
-def get_artist_track_timestamp(soup, lst, info_lst):
-    for val in lst:
-        info_lst.append(soup.find('input', {'name' : val})['value'])  
+def get_soup(index, user):
+    cookies, headers = cf.import_cookies_headers()
+    response = requests.get(f'https://www.last.fm/pt/user/{user}', cookies=cookies, headers=headers)
+    print(f"    Getting soup...")
+    try:
+        page = requests.get(f'https://www.last.fm/pt/user/{user}/library?page={index}', cookies=cookies, headers=headers)
+        print("    Success")
+    except:
+        print("    Failed getting soup")
+    return BeautifulSoup(page.content, "html.parser")
+
+def write_csv(soup):
+    tbodys = soup.find_all('tbody')[:-1]
+    with open("out.csv", "a", newline="") as f:
+        print("     Appending new information in csv")
+        writer = csv.writer(f)
+        for tbody in tbodys[::-1]:
+            for row in tbody.find_all('tr')[::-1]:
+                writer.writerow(main_info(row))
 
 def main_info(row):
     """main function to scrap lastfm scrobbles
@@ -46,22 +65,6 @@ def main_info(row):
     print(f"        Got: {itens}")
     return itens
 
-def write_csv(soup):
-    tbodys = soup.find_all('tbody')[:-1]
-    with open("out.csv", "a", newline="") as f:
-        print("     Appending new information in csv")
-        writer = csv.writer(f)
-        for tbody in tbodys[::-1]:
-            for row in tbody.find_all('tr')[::-1]:
-                writer.writerow(main_info(row))
-
-def get_soup(index, user):
-    cookies, headers = cf.import_cookies_headers()
-    response = requests.get(f'https://www.last.fm/pt/user/{user}', cookies=cookies, headers=headers)
-    print(f"    Getting soup...")
-    try:
-        page = requests.get(f'https://www.last.fm/pt/user/{user}/library?page={index}', cookies=cookies, headers=headers)
-        print("    Success")
-    except:
-        print("    Failed getting soup")
-    return BeautifulSoup(page.content, "html.parser")
+def get_artist_track_timestamp(soup, lst, info_lst):
+    for val in lst:
+        info_lst.append(soup.find('input', {'name' : val})['value'])  
